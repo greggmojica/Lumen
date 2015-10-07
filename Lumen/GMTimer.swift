c33c2908fb02174da6654c1d614c9b622543ee44
Gregg Mojica <GreggMojica@Greggs-MacBook.home>
2015-10-06 22:28:59 -0400

    first commit

---
+++ Lumen/GMTimer.swift
@@ -0,0 +1,13 @@
+//
+//  GMTimer.swift
+//  Lumen
+//
+//  Created by Gregg Mojica on 9/19/15.
+//  Copyright © 2015 Gregg Mojica. All rights reserved.
+//
+
+import UIKit
+
+class GMTimer: NSObject {
+
+}
